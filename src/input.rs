447eeae9c3447c5063367195798215ecc3f20423
Theodore DeRego <tderego94@gmail.com>
2014-10-13 16:21:01 -1000

    input system now uses channels instead of key/mouse handler interfaces

--- src/input.rs
+++ src/input.rs
@@ -1,36 +1,40 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use rsfml::window::{keyboard, mouse, event};
 use rsfml::graphics::RenderWindow;
 
-pub trait KeyHandler {
-    fn on_key_pressed(&mut self, key: keyboard::Key);
-    fn on_key_released(&mut self, key: keyboard::Key);
+pub enum KeyEvent {
+    KeyPressed(keyboard::Key),
+    KeyReleased(keyboard::Key),
 }
 
-pub trait MouseHandler {
-    fn on_mouse_button_pressed(&mut self, mouse::MouseButton);
-    fn on_mouse_button_released(&mut self, mouse::MouseButton);
+pub enum MouseEvent {
+    MouseButtonPressed(mouse::MouseButton),
+    MouseButtonReleased(mouse::MouseButton),
+    MouseMoved(i32, i32),
 }
-
-// Type aliases for boxed KeyHandler and MouseHandler
-pub type KeyHandlerBox = Box<KeyHandler + 'static>;
-pub type MouseHandlerBox = Box<MouseHandler + 'static>;
 
 pub struct InputSystem {
-    key_handlers: Vec<KeyHandlerBox>,
-    mouse_handlers: Vec<MouseHandlerBox>,
+    key_senders: Vec<Sender<KeyEvent>>,
+    mouse_senders: Vec<Sender<MouseEvent>>,
 }
 
 impl InputSystem {
     pub fn new() -> InputSystem {
-        InputSystem{key_handlers: vec!(), mouse_handlers: vec!()}
+        InputSystem{key_senders: vec!(), mouse_senders: vec!()}
     }
     
-    pub fn add_key_handler(&mut self, handler: KeyHandlerBox) {
-        self.key_handlers.push(handler);
+    pub fn add_key_receiver(&mut self) -> Receiver<KeyEvent> {
+        let (sender, receiver) = channel();
+        self.key_senders.push(sender);
+        receiver
     }
     
-    pub fn add_mouse_handler(&mut self, handler: MouseHandlerBox) {
-        self.mouse_handlers.push(handler);
+    pub fn add_mouse_receiver(&mut self) -> Receiver<MouseEvent> {
+        let (sender, receiver) = channel();
+        self.mouse_senders.push(sender);
+        receiver
     }
     
     pub fn update(&mut self, window: &mut RenderWindow) {
@@ -40,14 +44,14 @@ impl InputSystem {
                 event::KeyPressed{code, ..} => match code {
                     keyboard::Escape => {window.close(); break},
                     _ => {
-                        for handler in self.key_handlers.mut_iter() {
-                            handler.on_key_pressed(code);
+                        for sender in self.key_senders.iter() {
+                            sender.send(KeyPressed(code));
                         }
                     }
                 },
                 event::KeyReleased{code, ..} => {
-                    for handler in self.key_handlers.mut_iter() {
-                        handler.on_key_released(code);
+                    for sender in self.key_senders.iter() {
+                        sender.send(KeyReleased(code));
                     }
                 },
                 event::NoEvent => break,
