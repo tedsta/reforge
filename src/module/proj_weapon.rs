ed6f790dbe00f1f01fcaa40df088ef5f9ec6fdc2
Theodore DeRego <tderego94@gmail.com>
2014-10-13 14:09:58 -1000

    Made get_ship method on ModuleBase for convenience

--- src/module/proj_weapon.rs
+++ src/module/proj_weapon.rs
@@ -96,9 +96,7 @@ impl SimElement for ProjectileWeaponModule {
     }
     
     fn draw(&mut self, renderer: &mut Renderer, context: &BattleContext, simulating: bool, time: f32) {
-        let ship_index = self.base.index.as_ref().expect("Module not attached to ship").ship;
-        let ship = context.get_ship(&ship_index).expect(format!("Failed to get ship {}", ship_index).as_slice());
-        
+        let ship = self.base.get_ship(context);
         self.base.draw(renderer, ship);
         
         for projectile in self.projectiles.iter() {
