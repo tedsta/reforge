3d01ed5cdab0fb32c490a058196d7bc0847988a8
Theodore DeRego <tderego94@gmail.com>
2014-10-13 16:42:52 -1000

    Who needs an input system

--- src/space_gui.rs
+++ src/space_gui.rs
@@ -1,22 +1,32 @@
-use rsfml::window::keyboard;
+use rsfml::window::{keyboard, mouse, event};
+use rsfml::graphics::RenderWindow;
 
-use input::{InputSystem, KeyEvent, MouseEvent};
 use render::Renderer;
 
-pub struct SpaceGui {
-    key_receiver: Receiver<KeyEvent>,
-    mouse_receiver: Receiver<MouseEvent>,
-}
+pub struct SpaceGui;
 
 impl SpaceGui {
-    pub fn new(input: &mut InputSystem) -> SpaceGui {
-        SpaceGui {
-            key_receiver: input.add_key_receiver(),
-            mouse_receiver: input.add_mouse_receiver(),
-        }
+    pub fn new() -> SpaceGui {
+        SpaceGui
     }
     
-    pub fn update(&mut self) {
+    pub fn update(&mut self, window: &mut RenderWindow) {
+        loop {
+            match window.poll_event() {
+                event::Closed => window.close(),
+                event::KeyPressed{code, ..} => match code {
+                    keyboard::Escape => {},
+                    _ => {},
+                },
+                event::KeyReleased{..} => {},
+                event::MouseButtonPressed{button, x, y} => {
+                }
+                event::MouseButtonReleased{button, x, y} => {
+                }
+                event::NoEvent => break,
+                _ => {}
+            };
+        }
     }
     
     pub fn draw(&self, renderer: &mut Renderer) {
