12ec7b9cc663da486e7bf495163df4fa291ae93d
Theodore DeRego <tderego94@gmail.com>
2014-10-14 15:29:15 -1000

    progress on gui.. not really

--- src/space_gui.rs
+++ src/space_gui.rs
@@ -1,7 +1,8 @@
 use rsfml::window::{keyboard, mouse, event};
 use rsfml::graphics::RenderWindow;
 
-use render::Renderer;
+use module::MODULE_CATEGORIES;
+use render::{Renderer, LASER_TEXTURE};
 
 pub struct SpaceGui;
 
@@ -20,6 +21,10 @@ impl SpaceGui {
                 },
                 event::KeyReleased{..} => {},
                 event::MouseButtonPressed{button, x, y} => {
+                    match button {
+                        mouse::MouseLeft => self.on_mouse_left_pressed(x, y),
+                        _ => {},
+                    }
                 }
                 event::MouseButtonReleased{button, x, y} => {
                 }
@@ -30,5 +35,21 @@ impl SpaceGui {
     }
     
     pub fn draw(&self, renderer: &mut Renderer) {
+        for category in MODULE_CATEGORIES.iter() {
+            renderer.draw_texture(LASER_TEXTURE, 10.0 + (64.0*(category.id as u8 as f32)), 600.0);
+        }
+    }
+    
+    pub fn on_mouse_left_pressed(&mut self, x: i32, y: i32) {
+        for category in MODULE_CATEGORIES.iter() {
+            let icon_x = 10 + (64*(category.id as i32));
+            let icon_y = 600i32;
+            let icon_w = 64;
+            let icon_h = 64;
+            
+            if x >= icon_x && x <= icon_x+icon_w && y >= icon_y && y <= icon_y+icon_h {
+                println!("clicked!!");
+            }
+        }
     }
 }
