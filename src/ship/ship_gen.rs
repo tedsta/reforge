2c51ac07836b9ea62b3a5e18cd7e33fec325f7c6
Theodore DeRego <tderego94@gmail.com>
2014-11-30 22:33:41 -1000

    Changed ship a bit

--- src/ship/ship_gen.rs
+++ src/ship/ship_gen.rs
@@ -8,31 +8,36 @@ pub fn generate_ship(mod_store: &ModuleTypeStore, id: ShipId) -> Ship {
     engine.get_base_mut().x = 0;
     engine.get_base_mut().y = 0;
     
-    let mut weapon = ProjectileWeaponModule::new(mod_store, 1);
-    weapon.get_base_mut().x = 1;
-    weapon.get_base_mut().y = 1;
+    let mut weapon1 = ProjectileWeaponModule::new(mod_store, 1);
+    weapon1.get_base_mut().x = 3;
+    weapon1.get_base_mut().y = 1;
     
     let mut weapon2 = ProjectileWeaponModule::new(mod_store, 1);
-    weapon2.get_base_mut().x = 2;
-    weapon2.get_base_mut().y = 1;
+    weapon2.get_base_mut().x = 3;
+    weapon2.get_base_mut().y = 2;
     
-    let mut weapon3 = ProjectileWeaponModule::new(mod_store, 1);
-    weapon3.get_base_mut().x = 3;
-    weapon3.get_base_mut().y = 1;
+    let mut shield1 = ShieldModule::new(mod_store, 2);
+    shield1.get_base_mut().x = 2;
+    shield1.get_base_mut().y = 1;
     
-    let mut shield = ShieldModule::new(mod_store, 2);
-    shield.get_base_mut().x = 2;
-    shield.get_base_mut().y = 2;
+    let mut shield2 = ShieldModule::new(mod_store, 2);
+    shield2.get_base_mut().x = 2;
+    shield2.get_base_mut().y = 2;
     
-    let mut solar = SolarModule::new(mod_store, 3);
-    solar.get_base_mut().x = 1;
-    solar.get_base_mut().y = 2;
+    let mut solar1 = SolarModule::new(mod_store, 3);
+    solar1.get_base_mut().x = 1;
+    solar1.get_base_mut().y = 1;
+    
+    let mut solar2 = SolarModule::new(mod_store, 3);
+    solar2.get_base_mut().x = 1;
+    solar2.get_base_mut().y = 2;
     
     ship.add_module(engine);
-    ship.add_module(weapon);
+    ship.add_module(weapon1);
     ship.add_module(weapon2);
-    ship.add_module(weapon3);
-    ship.add_module(shield);
-    ship.add_module(solar);
+    ship.add_module(shield1);
+    ship.add_module(shield2);
+    ship.add_module(solar1);
+    ship.add_module(solar2);
     ship
 }
