4c3dfd7ed92f6747cc84f9e1179986ec2afb8c41
Theodore DeRego <tderego94@gmail.com>
2014-12-31 15:07:26 -1000

    AI works but there's a bug in module activation

--- run_game.sh
+++ run_game.sh
@@ -3,14 +3,10 @@ bin/server &
 sleep 3
 echo "Starting first client ..."
 bin/client localhost &
-sleep 1
-echo "Starting second client ..."
-bin/client localhost &
 
 sleep 1
 read -p "Press enter to terminate. " answer
 
 kill %1
 kill %2
-kill %3
 
