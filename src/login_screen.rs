d80dce1d76950f14a608e2db6728170b1c545b18
Theodore DeRego <tderego94@gmail.com>
2015-02-08 22:19:40 -1000

    TextBoxes get highlighted when you mouse-over and click them

--- src/login_screen.rs
+++ src/login_screen.rs
@@ -135,6 +135,15 @@ impl LoginScreen {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+#[derive(PartialEq)]
+enum MouseFocus {
+    NoHover,
+    Hover,
+    Focus,
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 struct TextButton {
     text: String,
     font_size: u32,
@@ -183,12 +192,16 @@ struct TextBox {
     text: String,
     font_size: u32,
     bg_color: [f32; 4],
+    bg_color_hover: [f32; 4],
+    bg_color_focus: [f32; 4],
     text_color: [f32; 4],
     
     position: [f64; 2],
     size: [f64; 2],
     
     pub has_focus: bool,
+    
+    mouse_focus: MouseFocus,
 }
 
 impl TextBox {
@@ -196,22 +209,33 @@ impl TextBox {
         TextBox {
             text: text,
             font_size: font_size,
-            bg_color: [0.3, 0.9, 0.0, 1.0],
+            bg_color: [0.3, 0.3, 1.0, 1.0],
+            bg_color_hover: [0.5, 0.5, 1.0, 1.0],
+            bg_color_focus: [0.0, 1.0, 0.0, 1.0],
             text_color: [1.0, 1.0, 1.0, 1.0],
             
             position: position,
             size: size,
             
             has_focus: false,
+            
+            mouse_focus: MouseFocus::NoHover,
         }
     }
     
     pub fn draw(&mut self, context: &Context, gl: &mut Gl, glyph_cache: &mut GlyphCache) {
         use graphics::*;
         use graphics::text::Text;
+        
+        let bg_color =
+            match self.mouse_focus {
+                MouseFocus::NoHover => self.bg_color,
+                MouseFocus::Hover => self.bg_color_hover,
+                MouseFocus::Focus => self.bg_color_focus,
+            };
     
         // Draw background rectangle
-        Rectangle::new(self.bg_color)
+        Rectangle::new(bg_color)
             .draw([self.position[0], self.position[1], self.size[0], self.size[1]], context, gl);
         
         // Draw text
@@ -228,7 +252,17 @@ impl TextBox {
         use event::*;
         
         e.mouse_cursor(|_, _| {
-            // TODO
+            if self.mouse_focus != MouseFocus::Focus {
+                let x = mouse_pos[0];
+                let y = mouse_pos[1];
+                if x >= self.position[0] && x <= self.position[0]+self.size[0] &&
+                    y >= self.position[1] && y <= self.position[1]+self.size[1]
+                {
+                    self.mouse_focus = MouseFocus::Hover;
+                } else {
+                    self.mouse_focus = MouseFocus::NoHover;
+                }
+            }
         });
         e.press(|button| {
             match button {
@@ -240,8 +274,10 @@ impl TextBox {
                             y >= self.position[1] && y <= self.position[1]+self.size[1]
                         {
                             self.has_focus = true;
+                            self.mouse_focus = MouseFocus::Focus;
                         } else {
                             self.has_focus = false;
+                            self.mouse_focus = MouseFocus::NoHover;
                         }
                     }
                 },
