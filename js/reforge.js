9c5ea736e47a1dc9f8bd17d3a220702bc23574dd
Brian Hall <brian@Brians-MacBook-Air.local>
2015-02-22 12:35:21 -1000

    added modal with windows/mac dl buttons

---
+++ js/reforge.js
@@ -0,0 +1,19 @@
+(function() {
+
+	var downloadScreen;
+
+	downloadScreen = function() {
+	var download_html;
+	download_html = "<h2>Choose your platform:</h2>";
+	download_html += '<a href="https://dl.dropboxusercontent.com/u/17256312/reForge.msi" type="application/octet-stream" class="small radius button">Windows</a><br/>';
+	download_html += '<a href="https://dl.dropboxusercontent.com/u/17256312/reforge_mac.tar.gz" type="application/octet-stream" class="small radius button">OSX</a>';
+	$('#download_text').html(download_html);
+	return $('#downloadScreen').foundation('reveal', 'open');
+	};
+
+
+	$("#download_button").click(function() {
+		downloadScreen();
+	});
+
+}).call(this);
