8cee41a3a176511e64adacea4f164fa7ab532381
Brian Hall <brian@reallymany.com>
2015-05-15 17:00:31 -0600

    updated download links

--- js/reforge.js
+++ js/reforge.js
@@ -5,8 +5,8 @@
 	downloadScreen = function() {
 	var download_html;
 	download_html = "<h2>Choose your platform:</h2>";
-	download_html += '<a href="https://dl.dropboxusercontent.com/u/17256312/reForge.msi" type="application/octet-stream" class="small radius button">Windows</a><br/>';
-	download_html += '<a href="https://dl.dropboxusercontent.com/u/17256312/reforge_mac.tar.gz" type="application/octet-stream" class="small radius button">OSX</a>';
+	download_html += '<a href="https://dl.dropboxusercontent.com/u/17256312/reforge_windows.zip" type="application/octet-stream" class="small radius button">Windows</a><br/>';
+	download_html += '<a href="https://dl.dropboxusercontent.com/u/17256312/reforge_mac.zip" type="application/octet-stream" class="small radius button">OSX</a>';
 	$('#download_text').html(download_html);
 	return $('#downloadScreen').foundation('reveal', 'open');
 	};
